a0849560ef187f8b64fd64be7cca3898ac93c14d
lukehasawii <may21luke@yahoo.com>
2019-09-18 21:56:58 -0500

    Initial openrc-turbo commit before merge

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotComponents/RobotComponent.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.RobotComponents;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public interface RobotComponent {
+    void init(HardwareMap hardwareMap);
+}
