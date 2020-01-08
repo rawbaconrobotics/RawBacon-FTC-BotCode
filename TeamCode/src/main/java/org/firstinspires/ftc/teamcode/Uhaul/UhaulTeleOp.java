0b4f97dae2a9526f82ff8cc9985faa9164e7fda6
snn1122 <54867875+snn1122@users.noreply.github.com>
2020-01-08 10:19:57 -0600

    javadoc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Uhaul/UhaulTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Uhaul/UhaulTeleOp.java
@@ -3,14 +3,18 @@ package org.firstinspires.ftc.teamcode.Uhaul;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-
+/**
+ * A linear opmode that activates the teleop stored in the {@link #Uhaul} class
+ * @author Luke Aschenbrener
+ */
 @TeleOp(name="UhaulTeleOp", group="Uhaul")
-
 public class  UhaulTeleOp extends UhaulLinearOpMode
 {
     private ElapsedTime runtime = new ElapsedTime();
 
-
+    /**
+     * Activates the teleop
+     */
     @Override
     public void run()
     {
