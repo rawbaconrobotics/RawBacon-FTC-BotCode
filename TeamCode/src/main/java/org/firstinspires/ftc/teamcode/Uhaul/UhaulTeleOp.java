b3ce7a212cd50d191be835500635d33de8e7f370
Luke <30738778+lukehasawii@users.noreply.github.com>
2020-01-08 16:52:38 -0600

    working on moving stuff to uhaul and adding the arm thing. Will need to finish moving, adding, and adding the arm to autonomous and teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Uhaul/UhaulTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Uhaul/UhaulTeleOp.java
@@ -4,13 +4,23 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
- * A linear opmode that activates the teleop stored in the {@link #Uhaul} class
+ * A linear opmode that activates the teleop stored in the {@link Uhaul} class
  * @author Luke Aschenbrener
  */
 @TeleOp(name="UhaulTeleOp", group="Uhaul")
 public class  UhaulTeleOp extends UhaulLinearOpMode
 {
     private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void on_init() {
+        robot.drive.init();
+        robot.latch.init();
+        robot.grabber.init();
+        robot.arm.init();
+        robot.intake.init();
+        robot.lift.init();
+    }
 
     /**
      * Activates the teleop
@@ -26,7 +36,13 @@ public class  UhaulTeleOp extends UhaulLinearOpMode
         while (opModeIsActive())
         {
             robot.teleOpActivated();
-
         }
     }
+
+    @Override
+    public void on_stop() {
+        robot.drive.stopDrive();
+    }
 }
+
+
