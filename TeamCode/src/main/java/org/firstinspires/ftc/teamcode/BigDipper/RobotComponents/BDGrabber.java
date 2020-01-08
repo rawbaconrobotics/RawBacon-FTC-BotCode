057e8429c1a57ec5d264e932ff99cbb7e88ca823
FireMiner99 <FireMiner99@users.noreply.github.com>
2020-01-08 17:33:03 -0600

    javadoc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BigDipper/RobotComponents/BDGrabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BigDipper/RobotComponents/BDGrabber.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode.BigDipper.RobotComponents;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -39,6 +10,12 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Represents the grabber  
+ * @author Raw Bacon Coders
+ */
+
 public class BDGrabber extends RobotComponentImplBase{
     private final static String GRABBER_SERVO_NAME = "tank_grabber";
     double GRABBER_OPEN = 0.9;
@@ -50,20 +27,34 @@ public class BDGrabber extends RobotComponentImplBase{
     private ElapsedTime runtime = new ElapsedTime();
     private CRServo grabberServo = null;
 
+    
+    /**
+     * Overrides the opMode method
+     */
     public BDGrabber(LinearOpMode opMode) {
         super(opMode);
     }
 
+     /**
+     * Initializes the grabber
+     */
     @Override
     public void init() {
         grabberServo = hardwareMap.crservo.get(GRABBER_SERVO_NAME);
     }
+    
+     /**
+     * Initializes the grabber
+     */
 
     @Override
     public void initAutonomous() {
         grabberServo = hardwareMap.crservo.get(GRABBER_SERVO_NAME);
     }
 
+     /**
+     * Opens or closes the grabber based upon the current state it is in
+     */
     public void grabber() {
         boolean openGrabber = gamepad1.right_bumper;
         boolean closeGrabber = gamepad1.left_bumper;
