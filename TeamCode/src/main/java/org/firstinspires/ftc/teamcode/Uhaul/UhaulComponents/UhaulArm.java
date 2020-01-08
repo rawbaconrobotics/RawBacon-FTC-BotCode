7f558eb74e9e159b3e9c0bfcbf82b10230522f3e
Luke <30738778+lukehasawii@users.noreply.github.com>
2020-01-07 18:03:46 -0600

    working on moving stuff to uhaul and adding the arm thing. Will need to finish moving, adding, and adding the arm to autonomous and teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Uhaul/UhaulComponents/UhaulArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Uhaul/UhaulComponents/UhaulArm.java
@@ -7,66 +7,85 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Uhaul.UhaulComponents.UhaulComponentImplBase;
 
-    public class UhaulArm extends UhaulComponentImplBase {
+public class UhaulArm extends UhaulComponentImplBase {
 
-        private ElapsedTime runtime = new ElapsedTime();
-        public CRServo uhaulSlider1 = null; //The slider for the arm
-        public CRServo uhaulSlider2 = null; //The thing that holds the block and rotates
-        public CRServo uhaulStoneFlipper = null; //The slider for the arm
-        public CRServo uhaulStoneGrabber = null; //The thing that holds the block and rotates
+    private ElapsedTime runtime = new ElapsedTime();
+    public Servo uhaulSlider1 = null; //The slider for the arm
+    public Servo uhaulSlider2 = null; //The thing that holds the block and rotates
+    public Servo uhaulStoneFlipper = null; //The slider for the arm
+    public Servo uhaulStoneGrabber = null; //The thing that holds the block and rotates
 
-        public UhaulArm(LinearOpMode opMode) {
-            super(opMode);
-        }
-
-        @Override
-        public void init() {
-            uhaulSlider1 = hardwareMap.crservo.get("uhaul_slider_1");
-            uhaulSlider2 = hardwareMap.crservo.get("uhaul_slider_2");
-            uhaulStoneFlipper = hardwareMap.crservo.get("uhaul_stone_flipper");
-            uhaulStoneGrabber = hardwareMap.crservo.get("uhaul_stone_grabber");
-
-        }
-
-        public void moveSlider() {
-            double sliderPower = -1;
-
-
-            double slider = gamepad2.left_stick_y;
-
-            slider = sliderPower;
-
-            if(((slider > -0.1) && (slider < 0.1)) ){
-                uhaulSlider1.setPower(0);
-                uhaulSlider2.setPower(0);
-            }
-            else{
-                uhaulSlider1.setPower(slider);
-                uhaulSlider2.setPower(slider);
-                slider = sliderPower;
-            }
-
-
-        }
-
-        public void sliderAuto(){
-
-        }
-        public void rotateAuto(){
-
-        }
-
-        public void grabAuto(){
-
-        }
-
-        @Override
-        public void initAutonomous() {
-
-            uhaulSlider1 = hardwareMap.crservo.get("uhaul_slider_1");
-            uhaulSlider2 = hardwareMap.crservo.get("uhaul_slider_2");
-            uhaulStoneFlipper = hardwareMap.crservo.get("uhaul_stone_flipper");
-            uhaulStoneGrabber = hardwareMap.crservo.get("uhaul_stone_grabber");
-
-        }
+    public UhaulArm(LinearOpMode opMode) {
+        super(opMode);
     }
+
+    @Override
+    public void init() {
+        uhaulSlider1 = hardwareMap.servo.get("uhaul_slider_1");
+        uhaulSlider2 = hardwareMap.servo.get("uhaul_slider_2");
+        uhaulStoneFlipper = hardwareMap.servo.get("uhaul_stone_flipper");
+        uhaulStoneGrabber = hardwareMap.servo.get("uhaul_stone_grabber");
+
+    }
+
+    public void moveSlider() {
+
+        double slider = gamepad2.left_stick_y;
+
+
+        if (((slider > -0.1) && (slider < 0.1))) {
+            //uhaulSlider1.setPower(0);
+            //uhaulSlider2.setPower(0);
+        } else {
+            //uhaulSlider1.setPower(slider);
+            //uhaulSlider2.setPower(slider);
+            //slider = sliderPower;
+        }
+
+
+    }
+
+    /*
+    public void moveSliderCR() {
+        double sliderPower = -1;
+
+
+        double slider = gamepad2.left_stick_y;
+
+        slider = sliderPower;
+
+        if (((slider > -0.1) && (slider < 0.1))) {
+            uhaulSlider1.setPower(0);
+            uhaulSlider2.setPower(0);
+        } else {
+            uhaulSlider1.setPower(slider);
+            uhaulSlider2.setPower(slider);
+            slider = sliderPower;
+        }
+
+
+    }
+*/
+    public void sliderAuto() {
+
+
+    }
+
+    public void rotateAuto() {
+
+    }
+
+    public void grabAuto() {
+
+    }
+
+    @Override
+    public void initAutonomous() {
+
+        uhaulSlider1 = hardwareMap.servo.get("uhaul_slider_1");
+        uhaulSlider2 = hardwareMap.servo.get("uhaul_slider_2");
+        uhaulStoneFlipper = hardwareMap.servo.get("uhaul_stone_flipper");
+        uhaulStoneGrabber = hardwareMap.servo.get("uhaul_stone_grabber");
+
+    }
+}
