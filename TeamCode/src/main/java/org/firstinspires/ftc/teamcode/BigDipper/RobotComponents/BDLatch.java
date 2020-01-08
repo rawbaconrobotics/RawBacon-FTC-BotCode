3e5ef01aec11de37f4c704f98b8ccb7e9c5e7f9f
FireMiner99 <FireMiner99@users.noreply.github.com>
2020-01-08 17:36:52 -0600

    javadoc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BigDipper/RobotComponents/BDLatch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BigDipper/RobotComponents/BDLatch.java
@@ -1,37 +1,13 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode.BigDipper.RobotComponents;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+ /**
+   * Represents the latch
+   * @author Raw Bacon Coders
+   */
 
 public class BDLatch extends RobotComponentImplBase {
 
@@ -44,20 +20,34 @@ public class BDLatch extends RobotComponentImplBase {
     private ElapsedTime runtime = new ElapsedTime();
     private Servo bdLatch = null;
 
+    
+     /**
+     * Overrides the opMode method
+     */
     public BDLatch(LinearOpMode opMode) {
         super(opMode);
     }
 
+     /**
+     * Initializes the latch
+     */
     @Override
     public void init() {
         bdLatch = hardwareMap.servo.get(LATCH_SERVO_NAME);
     }
 
+    
+     /**
+     * Initializes the latch
+     */
     public void initAutonomous(){
         bdLatch = hardwareMap.servo.get(LATCH_SERVO_NAME);
         bdLatch.setPosition(LATCH_OPEN);
     }
 
+     /**
+     * Defines latch positions to the gamepad
+     */
     public void latch() {
         boolean openLatch = gamepad2.y;
         boolean closeLatch = gamepad2.x;
@@ -70,12 +60,19 @@ public class BDLatch extends RobotComponentImplBase {
             //latchServo2.setPosition(LATCH_OPEN);
         }
     }
+    
+     /**
+     * Opens the latch
+     */
     public void openLatch(){
         System.out.println("LATCH OPENING");
         bdLatch.setPosition(LATCH_OPEN);
         //latchServo2.setPosition(LATCH_CLOSED);
     }
 
+     /**
+     * Closes the latch
+     */
     public void closeLatch(){
         System.out.println("LATCH CLOSING");
         bdLatch.setPosition(LATCH_CLOSED);
