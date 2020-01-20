ee61d26efdbdcb32c71d44bf4769d30f23f5b542
Revosportza <19693972+Revosportza@users.noreply.github.com>
2020-01-20 12:15:28 -0600

    javadoc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Uhaul/UhaulComponents/UhaulRobotComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Uhaul/UhaulComponents/UhaulRobotComponent.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Uhaul.UhaulComponents;
 
 /**
+ * @author Raw Bacon Coders
  * Provides a template for robot component classes
  */
 public interface UhaulRobotComponent {
